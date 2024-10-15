b7197ab45abe3fae5d8ca1f9a5aaf3ebefe3fc4b
lkendi <lkendi003@gmail.com>
2024-10-15 21:22:09 +0300

    promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,6 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-
   Promise.all([uploadPhoto(), createUser()])
     .then((uploadPhotoResponse, createUserResponse) => {
       console.log(`${uploadPhotoResponse.body} ${createUserResponse.firstName} ${createUserResponse.lastName}`);
