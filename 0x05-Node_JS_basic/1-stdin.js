5a0fa79906fd95d5c52497d38a852aa0b96270a1
lkendi <lkendi003@gmail.com>
2024-10-18 10:22:14 +0300

    NodeJS Basic

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,10 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (input) => {
-  const name = input.toString().trim();
-  if (name.endsWith('\n')) {
-    process.stdout.write(`Your name is: ${name.slice(0, -1)}\r\n`);
-    process.stdin.end();
-  }
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data.toString()}`);
+  process.exit();
 });
 
-process.stdin.on('end', () => {
+process.stdin.on('exit', () => {
   process.stdout.write('This important software is now closing\n');
 });
