b567cbccaccc3280720b7318058f24bd12434229
lkendi <lkendi003@gmail.com>
2024-10-18 08:40:27 +0300

    ES6-data manipulation

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,9 @@
 export default function cleanSet(set, startString) {
-  const filteredSet = [...set].filter((value) => value.startsWith(startString));
+  if (typeof startString !== 'string' || startString.length === 0) {
+    return '';
+  }
+
+  const filteredSet = [...set].filter((value) => value && value.startsWith(startString));
   const cleanedSet = filteredSet.map((value) => value.slice(startString.length)).join('-');
   return cleanedSet;
 }
